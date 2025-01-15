2a5c7edffb9217b701202df3225bc86aaca5b4c8
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2025-01-15 20:18:17 +0100

    Splitting components and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen m-0 p-0">
+    <main className="h-screen m-0 p-0 flex gap-8">
       <Sidebar />
+      {/* <NewProject /> */}
+
+      <NoProjectSelected />
     </main>
   );
 }
---
+++ src/components/Actions.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+function Actions() {
+  const Menu = styled.menu`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 1rem;
+    margin-block: 2rem;
+
+    & button {
+      padding: 0.5rem 1rem;
+      border: none;
+      border-radius: 0.25rem;
+      color: #fff;
+      font-weight: 600;
+      cursor: pointer;
+      transition: background-color 0.6s ease-in-out;
+    }
+
+    li:first-of-type button {
+      background-color: #dde6ed;
+    }
+
+    & li:first-of-type button:hover {
+      background-color: #b7c4cf;
+    }
+
+    li:not(:first-of-type) button {
+      background-color: #9db2bf;
+    }
+    & li:not(:first-of-type) button:hover {
+      background-color: #526d82;
+    }
+  `;
+
+  return (
+    <Menu>
+      <li>
+        <button>Cancel</button>
+      </li>
+      <li>
+        <button>Save</button>
+      </li>
+    </Menu>
+  );
+}
+
+export default Actions;
