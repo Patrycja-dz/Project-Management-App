21f71439d79e6c3973bd2f27532fab8a6a96cb59
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2025-01-16 20:38:19 +0100

    Collect user projects via state

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,42 @@
 import Input from "../components/Input";
-import React from "react";
+import React, { useState } from "react";
 import Actions from "./Actions";
 
 function NewProject() {
+  const [userInput, setUserInput] = useState({
+    title: "",
+    description: "",
+    dueDate: "",
+  });
+  const { title, description, dueDate } = userInput;
+  function handleChange(identifier, value) {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [identifier]: value,
+      };
+    });
+  }
   return (
     <div className="w-[35rem] mt-16">
       <Actions />
       <div>
-        <Input label="Title" />
-        <Input label="Description" Tag="textarea" />
-        <Input label="Due Date" />
+        <Input
+          label="Title"
+          value={title}
+          onChange={(e) => handleChange("title", e.target.value)}
+        />
+        <Input
+          label="Description"
+          Tag="textarea"
+          value={description}
+          onChange={(e) => handleChange("description", e.target.value)}
+        />
+        <Input
+          label="Due Date"
+          value={dueDate}
+          onChange={(e) => handleChange("dueDate", e.target.value)}
+        />
       </div>
     </div>
   );
